875f331b0dfbcb82f8bcc5e7425b3421b6a8754c
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-03 17:48:02 -1000

    PixelPainter Requirements met. Working on Fill function.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,22 +1,149 @@
-(function() {
-    let pixel = document.getElementById('pixelPainter');
-    let size = 50;
-    let row = 10;
-    let col = 10;
-    let table = document.createElement('TABLE');
+function pixelPainter(width, height) {
+  //function variables
+  let color = 0;
+  let size = 40;
+  let row = height;
+  let col = width;
+  let mousedown = false;
+  let fill = false;
 
-    for(let i = 0; i < row; i++){
-        let tempRow = document.createElement('tr');
-        tempRow.style.border = 'thin solid black';
-        for(let j = 0; j < col; j++){
-            let temp = document.createElement('td');
-            temp.style.width=size;
-            console.log(temp.width);
-            temp.style.height=size;
-            temp.style.border = 'thin solid black';
-            tempRow.append(temp);
+  //function created DOM elements
+  let pixel = document.getElementById('pixelPainter');
+  let table = document.createElement('Table');
+  table.id = 'pixelCanvas';
+  let tableDiv = document.createElement('div');
+  tableDiv.id = 'tableDiv';
+  let paletteDiv = document.createElement('div');
+  paletteDiv.id = 'paletteDiv';
+  let palette = document.createElement('Table');
+
+  //call my event listeners
+  eventHandlers();
+
+  //create and attach the palette
+  makeTable(palette, 20, 6, 15, getColor, undefined, setColors);
+  paletteDiv.append(palette);
+
+  //calls the function that makes all the buttons in pixel painter
+  makeButtons();
+
+
+
+  //create and attach the pixels
+  makeTable(table, row, col, size, setColor, eventColor, undefined, 'pixels');
+
+  pixel.append(paletteDiv);
+  tableDiv.append(table);
+  pixel.append(tableDiv);
+
+  //function methods to create the tables, modify the background color, and event handlers
+  function makeTable(tableToAppend, rows, cols, size, onclick, onhover, background, tdClass) {
+    for (let i = 0; i < rows; i++) {
+      let tempRow = document.createElement('tr');
+      for (let j = 0; j < cols; j++) {
+        let temp = document.createElement('td');
+        temp.style.width = size;
+        temp.style.height = size;
+        temp.style.border = 'thin solid black';
+        if (tdClass) {
+          temp.setAttribute('id', tdClass);
         }
-        table.appendChild(tempRow);
+        if (onclick) {
+          temp.addEventListener('click', onclick);
+          temp.addEventListener('mouseover', onhover);
+        }
+        if (background) {
+          temp.style.backgroundColor = background();
+        }
+        tempRow.append(temp);
+      }
+      tableToAppend.appendChild(tempRow);
     }
-    pixel.append(table);
-})()
+  }
+
+  function makeButtons() {
+    let erase = document.createElement('button');
+    erase.innerText = 'ERASE';
+    erase.addEventListener('click',function(){
+      color='white'
+    });
+  
+    let clear = document.createElement('button');
+    clear.innerText = 'CLEAR';
+    clear.addEventListener('click',function(){
+  
+      let clearCells = table.getElementsByTagName('td');
+  
+      for(let i = 0; i < clearCells.length; i++){
+        if(clearCells[i].style.backgroundColor !== 'white'){
+          clearCells[i].style.backgroundColor = 'white';
+        }
+      }
+    });
+
+    let fillBut = document.createElement('button');
+    fillBut.innerText = 'FILL';
+    fillBut.addEventListener('click',function(){
+      fill = true;
+    });
+
+    paletteDiv.append(fillBut);
+    paletteDiv.append(erase);
+    paletteDiv.append(clear);
+  }
+
+  function setColors() {
+    let red = parseInt(Math.random() * 255);
+    let green = parseInt(Math.random() * 255);
+    let blue = parseInt(Math.random() * 255);
+    const color = red.toString(16) + green.toString(16) + blue.toString(16);
+    return color;
+  }
+
+  function eventHandlers() {
+
+    document.addEventListener('mousedown', e => {
+      if (e.type === 'mousedown') {
+        mousedown = true;
+
+        if (e.target.id == 'pixels') {
+          setColor(e);
+        }
+
+      }
+    }, false);
+
+    document.addEventListener('mouseup', e => {
+      if (e.type === 'mouseup') {
+        mousedown = false;
+      }
+    }, false);
+  }
+
+  function eventColor(event) {
+    if(!fill){
+      if (mousedown) {
+        setColor(event);
+      }
+    }
+  }
+
+  function setColor(element) {
+    if(fill){
+      console.log(fill);
+      // fill = false;
+    }else {
+      if(element.target){
+        element.target.style.backgroundColor = color;
+      }
+    }
+    
+
+  }
+
+  function getColor() {
+    color = this.style.backgroundColor;
+  }
+}
+
+pixelPainter(25, 20);
